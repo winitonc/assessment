c538bf02fcbe77a6adea5899e1f559e6021e6d44
Winiton <winiton@central.tech>
2023-01-07 01:00:07 +0700

    Finish feature story 2 - get expense by ID API

--- server_integration_test.go
+++ server_integration_test.go
@@ -33,6 +33,7 @@ func init() {
 		h := expense.InitHandler(db)
 
 		e.POST("/expenses", h.CreateExpenseHandler)
+		e.GET("/expenses/:id", h.GetExpensesByIDHandler)
 		e.Start(fmt.Sprintf(":%d", serverPort))
 	}(eh)
 	for {
@@ -81,11 +82,37 @@ func TestCreateExpenseAPI(t *testing.T) {
 	if assert.NoError(t, err) {
 		assert.Nil(t, err)
 		assert.Equal(t, http.StatusCreated, resp.StatusCode)
-		// assert.Greater(t, exp.ID, 2)
-		// assert.Equal(t, "strawberry smoothie", exp.Title)
-		// assert.Equal(t, 13.26, exp.Amount)
-		// assert.Equal(t, "night market promotion discount 10 bath", exp.Note)
-		// assert.Equal(t, []string{"food", "beverage"}, exp.Tags)
+		assert.Equal(t, "strawberry smoothie", exp.Title)
+		assert.Equal(t, 13.26, exp.Amount)
+		assert.Equal(t, "night market promotion discount 10 bath", exp.Note)
+		assert.Equal(t, []string{"food", "beverage"}, exp.Tags)
 	}
 
+}
+
+func TestGetExpenseByID(t *testing.T) {
+	// Arrange
+	reqBody := ``
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://localhost:%d/expenses/2", serverPort), strings.NewReader(reqBody))
+	assert.NoError(t, err)
+	//req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+
+	// Act
+	var resp *http.Response
+	client := http.Client{}
+	resp, err = client.Do(req)
+	assert.NoError(t, err)
+
+	var byteBody []byte
+	byteBody, err = io.ReadAll(resp.Body)
+	assert.NoError(t, err)
+	resp.Body.Close()
+
+	// Assertions
+	expect := `{"id":2,"title":"Title IT","amount":10.99,"note":"Note IT","tags":["tagsIT1","tagsIT2"]}` + "\n"
+	if assert.NoError(t, err) {
+		assert.Nil(t, err)
+		assert.Equal(t, http.StatusOK, resp.StatusCode)
+		assert.Equal(t, expect, string(byteBody))
+	}
 }
